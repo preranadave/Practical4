a284b5fd10756e8244c9aeff747f625a46a747d0
preranadave <46474491+preranadave@users.noreply.github.com>
2019-04-16 23:35:30 +0530

    Add files via upload

---
+++ Practical-4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace Practical_4
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Customer(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+        public Customer()
+        {
+            this.ID = -1;
+            this.Name = string.Empty;
+        }
+        public void printID()
+        {
+            Console.WriteLine("ID is: {0}", this.ID);
+        }
+        public void printName()
+        {
+            Console.WriteLine("Name is: {0}", this.Name);
+        }
+    }
+    class Program
+    {
+        public static void Main(String[] a)
+        {
+            //Type T =    .GetType();
+            Type T = (typeof(Customer));
+
+            MethodInfo[] methods = T.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType + " " + method.Name);
+            }
+
+            PropertyInfo[] properties = T.GetProperties();
+
+            Console.WriteLine("\nProperties");
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType + " " + property.Name);
+            }
+            Console.WriteLine("\nConstructors");
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+    
